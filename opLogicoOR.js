2ff01f80a8d455046a2f0b49f87b4bed32dec0d0
CalixtoYuritzi <89166721+CalixtoYuritzi@users.noreply.github.com>
2023-03-05 18:36:22 -0600

    Create opLogicoOR.js

---
+++ opLogicoOR.js
@@ -0,0 +1,12 @@
+/*
+Evalúa si al menos una de las expresiones es verdadera.
+*/
+
+let edad = 25;
+let tieneIdentificacion = true;
+
+if(edad >= 18 || tieneIdentificacion) {
+    console.log("Puedes entrar la bar");
+} else{
+    console.log("No tienes la edad suficiente para entrar al bar");
+}
