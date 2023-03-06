ba5607aa17d0aa668485311e5b6133853c943bbc
CalixtoYuritzi <89166721+CalixtoYuritzi@users.noreply.github.com>
2023-03-05 18:35:20 -0600

    Create opLogicoAND.js

---
+++ opLogicoAND.js
@@ -0,0 +1,13 @@
+/*
+Evalúa si ambas expresiones son verdaderas.
+Si ambas expresiones son verdaderas, devuelve true.
+De lo contrario, devuelve false.
+*/
+let edad = 25;
+let mayorDeEdad = edad >= 18;
+
+if (edad >= 18 && mayorDeEdad) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Aún eres menor de edad");
+}
