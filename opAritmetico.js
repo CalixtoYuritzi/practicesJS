9498dfaea6882a91ddfbd244338fdfbbb1242c89
CalixtoYuritzi <89166721+CalixtoYuritzi@users.noreply.github.com>
2023-03-05 18:32:45 -0600

    Create opAritmetico.js

---
+++ opAritmetico.js
@@ -0,0 +1,14 @@
+//Suma
+console.log(1+2);
+
+//Resta
+console.log(5-2);
+
+//Multiplicación
+console.log(3*4);
+
+// División
+console.log(10/2);
+
+//Módulo (devuelve el resto de una división)
+console.log(10%3);
