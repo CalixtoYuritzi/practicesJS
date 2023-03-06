bef63b47d5096f1cef66b9d2d7e7508b5d1ec667
CalixtoYuritzi <89166721+CalixtoYuritzi@users.noreply.github.com>
2023-03-05 18:35:52 -0600

    Create opLogicoNOT.js

---
+++ opLogicoNOT.js
@@ -0,0 +1,10 @@
+/*
+Invierte el valor de la expresion. Si la expresión es verdadera, devuelve false, de lo contrario, devuelve true.
+*/
+
+let esFinDeSemana = true;
+if(!esFinDeSemana) {
+    console.log("A trabajar");
+} else {
+    console.log("A disfrutar del fin de semana");
+}
